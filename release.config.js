4ebc71d5343bdff320c8d28be83166c776838e57
pupueddy <pupueddy@gmail.com>
2023-03-26 16:52:47 -0300

    fix: Semantic versioning

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  branches: "main",
-  repositoryUrl: "https://github.com/pupueddy/actionstest.git",
-  plugins: [
+    branches: "main",
+    repositoryUrl: "https://github.com/pupueddy/actionstest.git",
+    plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
       '@semantic-release/git',
